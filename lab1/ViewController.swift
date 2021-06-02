de234f73d27b27536b6d995f5d3afdedd84217d4
unknown <dmytrenkos24@gmail.com>
2021-06-02 03:19:26 +0300

    additional task

--- lab1/ViewController.swift
+++ lab1/ViewController.swift
@@ -7,6 +7,20 @@
 
 import UIKit
 
+enum FactorialError: LocalizedError {
+    case lessZero
+    case iterationLimit
+    
+    var errorDescription: String? {
+        switch self {
+        case .lessZero:
+            return "passwed incorret number!"
+        default:
+            return "Iteration limit!"
+        }
+    }
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var textField: UITextField!
@@ -18,20 +32,27 @@ class ViewController: UIViewController {
             return
         }
         
-        guard let (x, y) = fermatFactor(of: number) else {
-            return
+        do {
+            let (x, y) = try fermatFactor(of: number)
+            
+            let ac = UIAlertController(title: "Have an answer!", message: "\(x) and \(y)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Great!", style: .default))
+            
+            present(ac, animated: true)
+        } catch {
+            let ac = UIAlertController(title: "Oppps!", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Great!", style: .default))
+            
+            present(ac, animated: true)
         }
         
-        let ac = UIAlertController(title: "Have an answer!", message: "\(x) and \(y)", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "Great!", style: .default))
-        
-        present(ac, animated: true)
+
     }
     
-    func fermatFactor(of n: Int) -> (Int, Int)? {
+    func fermatFactor(of n: Int) throws -> (Int, Int) {
 
         if n <= 0 {
-            return nil
+            throw FactorialError.lessZero
         }
         
         if n.isMultiple(of: 2) {
@@ -40,8 +61,12 @@ class ViewController: UIViewController {
         
         var a = Int(Double(n).squareRoot().rounded(.up))
         var b2 = a * a - n
-        
+        var iteration = 0
         while !b2.isSquare() {
+            iteration += 1
+            if iteration >= 100 {
+                throw FactorialError.iterationLimit
+            }
             a = a + 1
             b2 = a * a - n
         }
